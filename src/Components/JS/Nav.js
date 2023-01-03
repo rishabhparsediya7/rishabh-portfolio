3946822c68d504e8fbb72b291b0b2ae6b0f29b56
rishabhparsediya <parsediyarishabh@gmail.com>
2023-01-03 10:40:00 +0530

    push to git

---
+++ src/Components/JS/Nav.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "../CSS/Nav.css";
+import "../CSS/heading.css";
+const Nav = () => {
+  return (
+    <div className="navResponsive">
+      <nav role="navigation" style={{ position: "fixed" }}>
+        <div id="menuToggle">
+          <input type="checkbox" />
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <ul id="menu">
+            <a href="#">
+              <li>Home</li>
+            </a>
+            <a href="#experience">
+              <li>Experience</li>
+            </a>
+            <a href="#projects">
+              <li>Projects</li>
+            </a>
+            <a href="#certification">
+              <li>Certificates</li>
+            </a>
+            <a href="#education">
+              <li>Academic</li>
+            </a>
+            <a href="#contact">
+              <li>Contact</li>
+            </a>
+          </ul>
+        </div>
+        <div className="hr"></div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
