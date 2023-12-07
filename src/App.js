789b5d70a4ec2240f45f4d5aebe7218431f6faaa
Rishabh Parsediya <parsediyarishabh@gmail.com>
2023-12-07 18:48:24 +0530

    boiler plate for new design

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
-import Nav from "./Components/JS/Nav";
 import "./App.css";
-import ProfileContent from "./Components/JS/ProfileContent";
-import Experience from "./Components/JS/Experience";
-import Projects from "./Components/JS/Projects";
-import Education from "./Components/JS/Education";
-import Contact from "./Components/JS/Contact";
-import Footer from "./Components/JS/Footer";
-import Certification from "./Components/JS/Certification";
-import Skills from "./Components/JS/Skills";
-import "./Components/CSS/Responsive/responsive.css";
-import useLocalStorage from "use-local-storage";
-import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
-import Deck from "./Components/JS/Contact";
-function App() {
-  const defaultDark = window.matchMedia("(prefers-color-scheme: light)").matches;
-  const [theme, setTheme] = useLocalStorage(
-    "theme",
-    defaultDark ? "light" : "light"
-  );
 
-  const switchTheme = () => {
-    const newTheme = theme === "light" ? "light" : "light";
-    setTheme(newTheme);
-  };
-  return (
-    <div className="App" data-theme={theme}>
-      {/*  <button className="theme-button" onClick={switchTheme}>
-      //   {theme === "light" ? <BsFillMoonStarsFill/> : <BsFillSunFill/>}
-      // </button>*/}
-      <Nav />
-      <ProfileContent theme={theme}/>
-      <Experience />
-      <Projects />
-      <Skills />
-      <Education />
-      <Certification />
-      <Deck/>
-      <Footer />
-    </div>
-  );
+
+function App() {
+  return <div className="App">hie</div>;
 }
 
 export default App;
